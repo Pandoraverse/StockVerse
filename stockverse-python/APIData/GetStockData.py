2a98a75a30948ab662ad37da14c7a12a8f5a970c
Raj Agrawal <raj.agrawal1997@gmail.com>
2024-03-16 17:52:28 +0530

    Seed database

--- stockverse-python/APIData/GetStockData.py
+++ stockverse-python/APIData/GetStockData.py
@@ -4,14 +4,12 @@ import pandas as pd
 from StockEnum import ApiParameter
 import sys
 from datetime import datetime
-from sqlalchemy import create_engine, MetaData, Table
-from Read_Properties import load_properties
+from SeedDatabase import add_data_to_db
 
+dfs = {}
 stock_data = []
 from_date =  datetime.strptime(sys.argv[1], '%Y-%m-%d').date()
 to_date = datetime.strptime(sys.argv[2], '%Y-%m-%d').date()
-
-dfs = {}
 
 #Fetches the data from the API and stores it in a df and returns the df.
 def fetch_data_and_create_df(parameter, from_date, to_date):
@@ -41,21 +39,13 @@ def fetch_data_and_create_df(parameter, from_date, to_date):
 	except Exception as e:
 		print(e)
 		return None
-
-
-
-def add_data_to_db():
-	#use this line to get the connection string from the properties file and modify/delete this method as per the need.
-	conn_str = str(load_properties("conn_str"))
-
-
+            
 
 # This will loop through the ENUM and fetch and store the OLHC value as per the stock name in a dictionary.
 for key in ApiParameter:
-  df_name = f'{key.name}'  # Name of DataFrame
-  #df = fetch_data_and_create_df(key.value, from_date, to_date)
-  dfs[df_name] = df
+	df_name = f'{key.name}'  # Name of DataFrame
+	df = fetch_data_and_create_df(key.value, from_date, to_date)
+	dfs[df_name] = df
 
-# Print the df as per the stock name
+add_data_to_db(dfs);
 print(dfs)
-
