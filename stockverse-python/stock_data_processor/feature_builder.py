ab88c81328c8eea43c6f9027f1a93fe2cf9998ac
Raj Agrawal <raj.agrawal1997@gmail.com>
2024-07-13 20:29:31 +0530

    Calculate technical indicator using talipp

--- stockverse-python/stock_data_processor/feature_builder.py
+++ stockverse-python/stock_data_processor/feature_builder.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from talipp.indicators import RSI, MACD, BB
+from talipp.ohlcv import OHLCVFactory
 from database_handler import DatabaseHandler
 import StockEnum
 
@@ -8,25 +10,31 @@ def extract_technical_features(df):
     # 1. Moving Averages
     df['SMA_10'] = df['Close'].rolling(window=10).mean()
     df['EMA_10'] = df['Close'].ewm(span=10, adjust=False).mean()
+    df['SMA_30'] = df['Close'].rolling(window=30).mean()
+    df['EMA_30'] = df['Close'].ewm(span=30, adjust=False).mean()
+    df['SMA_50'] = df['Close'].rolling(window=50).mean()
+    df['EMA_50'] = df['Close'].ewm(span=50, adjust=False).mean()
+    df['SMA_100'] = df['Close'].rolling(window=100).mean()
+    df['EMA_100'] = df['Close'].ewm(span=100, adjust=False).mean()
 
     # 2. RSI (Relative Strength Index)
-    # df['RSI_14'] = talib.RSI(df['Close'].values, timeperiod=14)
+    df['RSI_14'] = RSI(period=14, input_values= df['Close'].values)
 
     # 3. MACD (Moving Average Convergence Divergence)
-    # macd, macdsignal, macdhist = talib.MACD(df['Close'].values, fastperiod=12, slowperiod=26, signalperiod=9)
-    # df['MACD'] = macd
-    # df['MACD_Signal'] = macdsignal
+    macd_values = MACD(fast_period=12, slow_period=26, signal_period=9, input_values= df['Close'].values)
+    df['MACD'] =  [obj.macd if obj is not None else np.nan for obj in macd_values]
+    df['MACD_Signal'] = [obj.signal if obj is not None else np.nan for obj in macd_values]
 
     # 4. Bollinger Bands
-    # upper_band, middle_band, lower_band = talib.BBANDS(df['Close'].values, timeperiod=20)
-    # df['BB_Upper'] = upper_band
-    # df['BB_Middle'] = middle_band
-    # df['BB_Lower'] = lower_band
+    bb= BB(period=20, std_dev_mult=5, input_values=df['Close'].values)
+    df['BB_Upper'] = [obj.lb if obj is not None else np.nan for obj in bb]
+    df['BB_Middle'] = [obj.ub if obj is not None else np.nan for obj in bb]
+    df['BB_Lower'] = [obj.cb if obj is not None else np.nan for obj in bb]
 
     # 5. Lagged Features (e.g., Previous day's closing price)
-    # df['Prev_Close'] = df['Close'].shift(1)
+    df['Prev_Close'] = df['Close'].shift(1)
 
-    # 6. Example of sentiment feature (hypothetical)
+    # 6. Example of sentiment feature (hypothetical)  -- This can't be implemented right now but will be in future.
     # Assume sentiment_score is a hypothetical sentiment score from NLP analysis
     # df['Sentiment_Score'] = sentiment_score
 
